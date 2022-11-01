72e8bc7f955f62a7d338b4c8ed8eaf149f2b294e
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-10-31 21:57:08 -0300

    Add FractionallySizedBox sample - This widget can make a exactly size of screen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fractionally_sized_box
+
+<div align='center'>
+  <img src='https://github.com/phferreira/assets/blob/master/images/fractionally_sized_box.png'/>
+</div>
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fractionally SizedBox',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 74),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fractionally SizedBox'),
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.5,
+          child: Container(
+            color: Colors.orangeAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
